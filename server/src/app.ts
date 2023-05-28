6096871c9deea118bf60f79a445623e230a4a292
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-05-28 19:15:43 +0200

    ADD: auth user,Route api

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 
 import errorHandler from "./middlewares/errorHandler";
 import { apiLimiter } from "./middlewares/rateLimit";
+import authRoute from "./routes/auth.routes";
+import userRoute from "./routes/user.routes";
 
 const app = express();
 
@@ -22,6 +24,8 @@ app.use(cookieParser());
 app.use(apiLimiter);
 
 // api routes
+app.use("/auth", authRoute);
+app.use("/user", userRoute);
 
 // error handler
 app.use(errorHandler);
