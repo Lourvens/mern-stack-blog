92e85d0f385b2a5b1d1ad600b6094fc716d93223
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-06-06 17:14:20 +0200

    documented the useCredential server hook

--- server/src/hooks/useCredential.ts
+++ server/src/hooks/useCredential.ts
@@ -2,9 +2,20 @@ import jwt from "jsonwebtoken";
 import { Request } from "express";
 import { authTokenPayload } from "../../../types";
 
+/**
+ * this function is use to obtain the user credentials stored in the `req.header.authorization` and return it
+ * as a baerer token. make sure, `useCredentials` is called in a `protected route` by the `isAuthenticated` middleware.
+ * otherwise an Error will be thrown
+ */
+
 function useCredentials(req: Request<any, any, any, any>) {
-  let token = req.headers["authorization"]?.split(" ")[1] as string;
+  const token = req.headers["authorization"]?.split(" ")[1] as string;
   const credentials = jwt.decode(token) as authTokenPayload;
+
+  if (!credentials) {
+    throw new Error("token incorrect or null");
+  }
+
   return credentials;
 }
 
