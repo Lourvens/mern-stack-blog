d48f99d08cc6d2552814544b2bef2311bceede90
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-06-11 19:28:29 +0200

    remove tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,33 +6,13 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        layout: {
-          900: "#252935",
-          700: "#313442",
-          600: "#3C3D46",
-          500: "#515765"
-        },
-        primary: {
-          700: "#2C4768",
-          600: "#268CE5",
-          500: "#1A87EF",
-        }
-      }
+      colors: {}
     },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#268CE5",
-          accent: "#268CE5",
-          neutral: "#3d4451",
-          "base-100": "#252935",
-        },
-      },
-    ],
+    themes: false
   },
 }
 
