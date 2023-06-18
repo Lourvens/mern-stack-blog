c9c735ba84b7e40db559fca18efc3c7e7eca291e
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-06-18 21:35:56 +0200

    fix tsc lint error

--- client/src/pages/HomePage/components/CardSkeleton.tsx
+++ client/src/pages/HomePage/components/CardSkeleton.tsx
@@ -4,7 +4,7 @@ function CardSkeleton() {
       {Array.from(Array(6)).map((item, index) => (
         <div
           className="animate-pulse flex bg-gray-200 dark:bg-gray-700 p-2 rounded-xl"
-          key={`${index}`}
+          key={`${item}${index}`}
         >
           <div className="flex gap-3 w-full"></div>
           <div className="shrink-0 w-24 h-24 rounded-xl bg-slate-400"></div>
