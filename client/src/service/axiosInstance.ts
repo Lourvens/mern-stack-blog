9c87dd4c71da80a544e7e0a89e4b34191cd3a454
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-06-26 20:31:48 +0200

    Enhance error catching, add axios interceptor for authentication

--- client/src/service/axiosInstance.ts
+++ client/src/service/axiosInstance.ts
@@ -1,4 +1,4 @@
-import axios, { isAxiosError } from "axios";
+import axios, { AxiosRequestConfig, isAxiosError } from "axios";
 import {
   BAD_REQUEST_ERR,
   CONFLICT_ERR,
@@ -7,36 +7,67 @@ import {
   UNAUTHORIZED_ERR,
   TOO_MANY_REQUESTS_ERR,
 } from "@/utils/httpError";
+import authService from "@/features/Auth/auth.service";
 
 export const axiosInstance = axios.create({
   baseURL: "/api/",
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use(null, (err) => {
-  if (isAxiosError(err)) {
-    switch (err.response?.status) {
-      case 400: // 400
-        throw new BAD_REQUEST_ERR(err.message);
-      case 401: //401
-        throw new UNAUTHORIZED_ERR(err.message);
-      case 403: // 403
-        throw new FORBIDDEN_ERR(err.message);
-      case 404: // 404
-        throw new NOT_FOUND_ERR(err.message);
-      case 409:
-        throw new CONFLICT_ERR(err.message);
-      case 429:
-        throw new TOO_MANY_REQUESTS_ERR(err.message);
+/**
+ * Auto send access token in each request if the token was in localstorage
+ */
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+    config.withCredentials = true;
+  }
+  return config;
+});
+
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  __isRetryReq: boolean;
+}
+/**
+ * refresh the access token when request failed with 401 status code
+ * and retry the request again
+ */
+axiosInstance.interceptors.response.use(null, async (error) => {
+  if (isAxiosError(error)) {
+    const prevRequest = error.config as CustomAxiosRequestConfig;
+    const errorMessage = error.response?.data.error.message as string;
+
+    if (error.response?.status === 401 && !prevRequest?.__isRetryReq) {
+      console.log(errorMessage);
+      const isInvalidRefreshToken = /token/.test(errorMessage);
+      if (isInvalidRefreshToken) return Promise.reject(error);
+
+      prevRequest.__isRetryReq = true;
+      const { data } = await authService.refreshToken();
+      localStorage.setItem("token", data.access_token);
+
+      return axiosInstance(prevRequest);
     }
+    return Promise.reject(error);
   }
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  config.headers.Authorization = `Bearer ${token}`;
-  config.withCredentials = true;
-
-  return config;
+axiosInstance.interceptors.response.use(null, (err) => {
+  if (isAxiosError(err)) {
+    switch (err.response?.status) {
+      case 400:
+        throw new BAD_REQUEST_ERR(err);
+      case 401:
+        throw new UNAUTHORIZED_ERR(err);
+      case 403:
+        throw new FORBIDDEN_ERR(err);
+      case 404:
+        throw new NOT_FOUND_ERR(err);
+      case 409:
+        throw new CONFLICT_ERR(err);
+      case 429:
+        throw new TOO_MANY_REQUESTS_ERR(err);
+    }
+  }
 });
--- client/src/utils/httpError.ts
+++ client/src/utils/httpError.ts
@@ -1,17 +1,33 @@
+import { AxiosError } from "axios";
+type Error = AxiosError<unknown, unknown>;
+
+class HttpError extends Error {
+  public config;
+  public response;
+  public request;
+
+  constructor(error: Error) {
+    super(error.message, error.cause);
+    this.config = error.config;
+    this.response = error.response;
+    this.request = error.request;
+  }
+}
+
 // status code 404
-export class NOT_FOUND_ERR extends Error {}
+export class NOT_FOUND_ERR extends HttpError {}
 
 // status code 400
-export class BAD_REQUEST_ERR extends Error {}
+export class BAD_REQUEST_ERR extends HttpError {}
 
 // status code 401
-export class UNAUTHORIZED_ERR extends Error {}
+export class UNAUTHORIZED_ERR extends HttpError {}
 
 // status code 403
-export class FORBIDDEN_ERR extends Error {}
+export class FORBIDDEN_ERR extends HttpError {}
 
 // status code 409
-export class CONFLICT_ERR extends Error {}
+export class CONFLICT_ERR extends HttpError {}
 
 // status code 429
-export class TOO_MANY_REQUESTS_ERR extends Error {}
+export class TOO_MANY_REQUESTS_ERR extends HttpError {}
