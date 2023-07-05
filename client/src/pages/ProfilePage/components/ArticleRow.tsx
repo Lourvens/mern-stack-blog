9e70a89661599be1acb07e7c181f75055a414718
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-07-05 17:42:37 +0200

    update ProfilePage

--- client/src/pages/ProfilePage/components/ArticleRow.tsx
+++ client/src/pages/ProfilePage/components/ArticleRow.tsx
@@ -1,7 +1,9 @@
 import ArticleList from "@/components/ArticleList";
 import useAuth from "@/hooks/useAuth";
 import ArticleService from "@/service/api/articleService";
+import { APP_ROUTE } from "@/utils/constants";
 import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
 
 function ArticleRow() {
   const { credential } = useAuth();
@@ -24,6 +26,14 @@ function ArticleRow() {
         isLoading={isLoading}
         withBtnAction={true}
       />
+      {isFetched && data && (
+        <div className="text-2xl my-6">
+          You've written no article{" "}
+          <Link to={APP_ROUTE.ARTICLE_EDIT} className="link">
+            write one now
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
